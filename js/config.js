ca1057ddec3f21da5ab5eb1169299a49d7d950a2
nayana266 <66182626+nayana266@users.noreply.github.com>
2025-04-25 07:53:22 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-2_sJm7g3dTG', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7kdg73h2kvag4gohk0ccb77pam', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '38duc20tar53gi5l80ef3vrc9r', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
